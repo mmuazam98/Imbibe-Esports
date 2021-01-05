68d5b9fa3ce71a216fd9e5aa09b5b83e0f894273
Mohammad MuaZam <mm9084@srmist.edu.in>
2021-01-05 23:34:21 +0530

    updated

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ info.forEach((s, index) => {
           <div class="contentBx">
             <p class="extName" style="--i:2">${s.name}</p>
             <h5 class="mt-3 name" style="--i: 2">${s.name}</h5>
-            <h5 class="span votes" style="--i: 3"></h5>
+            <h5 class="span" style="--i: 3"></h5>
           </div>
           <button class="btn btn-white btn-animate" id=${s.id} style="--i: 2.5">Vote</button>
         </div>
